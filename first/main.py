756c288799e8f70b4ddd10f00f87e3f3f6153b4c
jamleston <jamlessston@gmail.com>
2024-07-11 16:28:45 +0200

    first task

---
+++ first/main.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+def summary (arr):
+    sum = 0
+    for num in arr:
+        sum = sum + num
+    return sum
+
+def median (arr):
+    sum = summary(arr)
+    num_of_workers = len(arr)
+    median = sum/num_of_workers
+    return median
+
+def total_salary(path):
+    try:
+        with open(path, "r") as file:
+            workers = [el.strip() for el in file.readlines()]
+            salaries = []
+            for worker in workers:
+                worker_data = worker.split(',')
+                salaries.append(int(worker_data[1]))
+            sum = summary(salaries)
+            med = int(median(salaries))
+            result_tuple = (sum, med)
+        return result_tuple
+    except (TypeError, FileNotFoundError):
+        answer = 'path is not correct'
+        print(answer)
+        return False, False
+
+p = Path("C:\\Users\\kasht\\OneDrive\\Documents\\GitHub\\python_modul4_hw\\first\\data.txt")
+
+result = total_salary(p)
+print(f'total: {result[0]}, average: {result[1]}')
