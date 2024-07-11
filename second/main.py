4dc16874b60c0bc86c95fc671dd9149e21943692
jamleston <jamlessston@gmail.com>
2024-07-11 17:18:23 +0200

    add data checker for second task

--- second/main.py
+++ second/main.py
@@ -7,12 +7,16 @@ def get_cats_info(path):
             cats_list = []
             for cat in cats:
                 cat_data = cat.split(',')
-                cat_dict = {'id': cat_data[0], 'name': cat_data[1], 'age': cat_data[2]}
-                cats_list.append(cat_dict)
+                if len(cat_data) == 3:
+                    cat_dict = {'id': cat_data[0], 'name': cat_data[1], 'age': cat_data[2]}
+                    cats_list.append(cat_dict)
+                else:
+                    answer = 'data is not complete'
+                    return answer
         return cats_list
     except (TypeError, FileNotFoundError):
         answer = 'path is not correct'
-        return(answer)  
+        return answer
 
 p = Path("C:\\Users\\kasht\\OneDrive\\Documents\\GitHub\\python_modul4_hw\\second\\data.txt")
 result = get_cats_info(p)
